fbd24f7769e0042f8209204a9e82141699f37bdd
Bjørn Esaiassen <bfe@icsys.no>
2019-10-17 14:15:05 +0200

    Added Example

---
+++ MongoDbClientExample/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using MongoDB.Client.Simple;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using System;
+
+namespace MongoDbClientExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MongoDbContextBuilder()
+                .SetOptions(opt =>
+                {
+                    opt.ConnectionString = "<your connection string>";
+                    opt.DatabaseName = "<your database>";
+                    opt.MaxLifeTime = 1;
+                    opt.MaxIdleTime = 1;
+                })
+                .Build();
+
+
+            IMongoCollection<TestDatabaseModel> modelCollection = context.GetCollection<TestDatabaseModel>("testModels");
+
+            var filter = Builders<TestDatabaseModel>.Filter.Empty;
+            var models = modelCollection
+                .Find(filter)
+                .ToList();
+
+            foreach(var m in models)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(m));
+            }
+
+            var newModel = new TestDatabaseModel()
+            {
+                Name = "TestModel"
+            };
+
+            modelCollection.InsertOne(newModel);
+
+
+        }
+    }
+}
---
+++ MongoDbClientExample/TestDatabaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoDbClientExample
+{
+    public class TestDatabaseModel
+    {
+        public Guid ModelID { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+    }
+}
